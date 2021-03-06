80f8b22fe8912fef6a1665945550b0abd0cffc72
RobBlumberg <robertblumberg97@gmail.com>
2021-03-06 15:46:56 -0500

    formatting

--- fantasy_stats/fantasy_stats/player_stats.py
+++ fantasy_stats/fantasy_stats/player_stats.py
@@ -5,20 +5,18 @@ def get_player_stats(team):
     """
     team : espn_api Team object
     """
-    stats_df = pd.DataFrame({
-        player.name : player.stats['002021']['avg']  
-        for player in team.roster
-    }).T
+    stats_df = pd.DataFrame(
+        {player.name: player.stats["002021"]["avg"] for player in team.roster}
+    ).T
 
     stats_total = stats_df.sum()
     stats_total["FG%"] = stats_total["FGM"] / stats_total["FGA"]
     stats_total["FT%"] = stats_total["FTM"] / stats_total["FTA"]
 
-    keep_stats = [
-        "PTS", "BLK", "STL", "FG%", "FT%", "REB", "AST", "3PTM", "TO"
-    ]
+    keep_stats = ["PTS", "BLK", "STL", "FG%", "FT%", "REB", "AST", "3PTM", "TO"]
 
     return stats_total[keep_stats]
+
 
 def matchup_comparison(team1, team2):
 
@@ -26,10 +24,10 @@ def matchup_comparison(team1, team2):
     stats_team2 = get_player_stats(team2)
 
     match_stats = pd.merge(
-        stats_team1.to_frame(), 
+        stats_team1.to_frame(),
         stats_team2.to_frame(),
         left_index=True,
-        right_index=True
+        right_index=True,
     )
     match_stats.columns = [team1.team_name, team2.team_name]
 
