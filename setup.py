c2b54d733127fa2714bcc036b83f58d30d50871e
RobBlumberg <robertblumberg97@gmail.com>
2021-03-06 17:47:18 -0500

    add setup and bump

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name="espn_fantasy_matchup_stats", # Replace with your own username
+    version="0.3.0",
+    author="Rob Blumberg",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires='>=3.7',
+)
