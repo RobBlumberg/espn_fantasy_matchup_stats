38eaf7bb62fb7813c10532a45f0e99941e500b8b
RobBlumberg <robertblumberg97@gmail.com>
2021-03-06 17:58:04 -0500

    version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="espn_fantasy_matchup_stats",  # Replace with your own username
-    version="0.3.0",
+    version="0.4.0",
     author="Rob Blumberg",
     packages=setuptools.find_packages(),
     include_package_data=True,
