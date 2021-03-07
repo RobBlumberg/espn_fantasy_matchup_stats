dc87a2ded36a537ba0b74323632e810b0ff7b203
RobBlumberg <robertblumberg97@gmail.com>
2021-03-07 14:31:09 -0500

    intantiate league in auth file

--- espn_fantasy_matchup_stats/auth.py
+++ espn_fantasy_matchup_stats/auth.py
@@ -1,4 +1,5 @@
 import os
+from espn_api.basketball import League
 
 
 class Auth:
@@ -11,3 +12,12 @@ class Auth:
         self.LEAGUE_ESPN_S2 = os.environ.get("LEAGUE_ESPN_S2")
         self.LEAGUE_ID = int(os.environ.get("LEAGUE_ID", "0"))
         self.LEAGUE_YEAR = int(os.environ.get("LEAGUE_YEAR", "0"))
+
+auth=Auth()
+
+my_league = League(
+    league_id=auth.LEAGUE_ID,
+    year=auth.LEAGUE_YEAR,
+    espn_s2=auth.LEAGUE_ESPN_S2,
+    swid=auth.LEAGUE_SWID,
+)
