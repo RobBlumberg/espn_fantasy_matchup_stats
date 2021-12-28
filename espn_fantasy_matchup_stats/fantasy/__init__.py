47fb6e156b8e96895b650cf025159bea3f850f61
RobBlumberg <robertblumberg97@gmail.com>
2021-12-28 18:46:49 -0500

    fix imports

--- espn_fantasy_matchup_stats/fantasy/__init__.py
+++ espn_fantasy_matchup_stats/fantasy/__init__.py
@@ -1,4 +1,4 @@
-from .my_team import MyTeam
-from .auth import Auth
+from espn_fantasy_matchup_stats.fantasy.auth import my_league
+from espn_fantasy_matchup_stats.fantasy.my_team import MyTeam
 
 __all__ = ["MyTeam", "my_league"]
